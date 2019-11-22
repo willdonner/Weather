24a172f89250d797771782c8132b65baf91ddc90
lichunkaik <57978568+lichunkaik@users.noreply.github.com>
2019-11-22 11:39:24 +0800

    更改背景显示

---
+++ app/src/main/java/com/dongxun/lichunkai/weather/NetworkReceiver.java
@@ -0,0 +1,10 @@
+package com.dongxun.lichunkai.weather;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+
