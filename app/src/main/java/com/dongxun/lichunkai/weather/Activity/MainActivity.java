2e7bc1326b5ad9d797bde7780070a17dd191e38f
lichunkaik <57978568+lichunkaik@users.noreply.github.com>
2019-12-04 16:51:22 +0800

    Update MainActivity.java

--- app/src/main/java/com/dongxun/lichunkai/weather/Activity/MainActivity.java
+++ app/src/main/java/com/dongxun/lichunkai/weather/Activity/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationListener;
 import android.location.LocationManager;
